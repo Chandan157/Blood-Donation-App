c43d8c43d046ed15dcbadb483fe93b8a656d6ae5
Chandan <94438598+Chandan157@users.noreply.github.com>
2024-04-30 22:54:06 +0530

    homepage,login,register page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 
-
+import {Routes,Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 function App() {
   return (
     <div>
-      <h1>Blood Bank App</h1>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
     </div>
   );
 }
