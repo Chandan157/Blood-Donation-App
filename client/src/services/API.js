76a1f8646862895d375a67432dd6e5f3c1127276
Chandan <94438598+Chandan157@users.noreply.github.com>
2024-05-02 01:11:28 +0530

    register and login slice added

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItems("token")) {
-    req.headers.Authorisation = `Bearer ${localStorage.getItem("token")}`;
+  if (localStorage.getItem("token")) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem("token")} `;
   }
   return req;
 });
